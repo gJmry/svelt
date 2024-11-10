c6854ec8cef61da195ea5c67572caa76b33137d3
Jérémy Girard <oqaislenning@gmail.com>
2024-11-10 10:45:03 +0100

    fix: check for first arg 🐛

--- src/cli.rs
+++ src/cli.rs
@@ -3,10 +3,14 @@ mod help;
 
 pub fn run(args: Vec<String>) {
     if !args.is_empty() {
-        match args[1].as_str() {
-            "init" => project_creation::main(args),
-            "create" => project_creation::main(args),
-            _ => {help::main().expect("Error while fetching help");}
+        if args[1].is_empty() {
+            help::main().expect("Error while fetching help");
+        } else {
+            match args[1].as_str() {
+                "init" => project_creation::main(args),
+                "create" => project_creation::main(args),
+                _ => { help::main().expect("Error while fetching help"); }
+            }
         }
     }
 }

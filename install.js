e7e02560420a3b5a268d7b6b4d16f6d0900b7150
Jérémy Girard <oqaislenning@gmail.com>
2025-01-07 17:09:22 +0100

    feat: update install js for other os

--- install.js
+++ install.js
@@ -1,11 +1,20 @@
 const fs = require('fs');
 const path = require('path');
+const os = require('os');
 
-const binPath = path.resolve(__dirname, 'bin', 'svelt.exe');
+let binPath;
+
+if (os.platform() === 'win32') {
+    binPath = path.resolve(__dirname, 'bin', 'svelt.exe');
+} else if (os.platform() === 'linux') {
+    binPath = path.resolve(__dirname, 'bin', 'svelt-linux');
+} else if (os.platform() === 'darwin') {
+    binPath = path.resolve(__dirname, 'bin', 'svelt-mac');
+}
 
 if (fs.existsSync(binPath)) {
-    console.log('svelt.exe has been installed successfully!');
+    console.log('svelt has been installed successfully!');
 } else {
-    console.error('Error: svelt.exe is missing.');
+    console.error('Error: svelt binary is missing.');
     process.exit(1);
 }

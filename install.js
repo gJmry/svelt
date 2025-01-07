7cf92fbf6612f1ba173225785528d0e0c975cf6f
Jérémy Girard <oqaislenning@gmail.com>
2025-01-07 23:42:17 +0100

    feat: update js

--- install.js
+++ install.js
@@ -5,9 +5,13 @@ const os = require('os');
 let binPath;
 
 if (os.platform() === 'win32') {
-    binPath = path.resolve(__dirname, 'bin', 'svelt.exe');
+    if (os.arch() === 'x64') {
+        binPath = path.resolve(__dirname, 'bin', 'svelt-win64.exe');
+    } else if (os.arch() === 'ia32') {
+        binPath = path.resolve(__dirname, 'bin', 'svelt-win32.exe');
+    }
 } else if (os.platform() === 'linux') {
-    binPath = path.resolve(__dirname, 'bin', 'svelt-linux');
+    binPath = path.resolve(__dirname, 'bin', 'svelt-linux-x86_64');
 } else if (os.platform() === 'darwin') {
     binPath = path.resolve(__dirname, 'bin', 'svelt-mac');
 }
